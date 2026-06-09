db8705c94629b7fdf889c058cb7974b4af8f660b
unknown-entity98 <sam.navuduri@gmail.com>
2026-06-09 17:36:25 +1000

    adding a manual decision router for the RAG chatbot

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 app = FastAPI()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
+MODEL_NAME = os.getenv("MODEL_NAME")
 class QueryRequest(BaseModel):
     user: str
     query: Annotated[str, Field(min_length=1)]
@@ -22,14 +22,39 @@ class QueryResponse(BaseModel):
 def check_status():
     return {"status": "ok"}
 
-@app.post('/ask')
-def ask_question(request: QueryRequest) -> QueryResponse:
+# helper functions
+def route_query(query: str) -> str:
+    model_resp = client.chat.completions.create(
+         model = MODEL_NAME,
+        messages = [{'role': 'system', 'content':'you are a decision router. you will look at the query and decide if you need to look up the documents. give only either YES/NO'},
+        {'role':'user','content':query}]
+)
+    return model_resp.choices[0].message.content.strip().upper()
+
+def retrieve_and_answer(query: str) -> str:
+    return "RAG yet to be implemented"
+
+def answer_directly(query: str) -> str:
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content":request.query} 
+        model = MODEL_NAME,
+        messages = [
+            {'role':'system', 'content': 'you are a helpful assistant who answers in the simplest way'},
+            {'role':'user', 'content': query}
         ]
     )
-    answer = response.choices[0].message.content
+    return response.choices[0].message.content
+
+
+# query function
+@app.post('/ask')
+def ask_question(request: QueryRequest) -> QueryResponse:
+    decision = route_query(request.query)
+    print(f'Query:{request.query} --> decision: {decision}') # debugging
+    if 'YES' in decision:
+        answer = retrieve_and_answer(request.query)
+    elif 'NO' in decision:
+        answer = answer_directly(request.query)
+    else:
+        answer = answer_directly(request.query) # this is the default fallback
+
     return QueryResponse(ans=answer)
